20e35423b2960a2c56703916f04c0aa5c89166e3
Marcelo Macedo <marcelomacedo17@gmail.com>
2021-09-25 19:34:30 +0100

    Created Landmark Details

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,22 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Marcelo Macedo on 19/09/2021.
+//
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
