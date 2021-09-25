ce35b5106e494d19759ed4a22d08cb82ae5e21fc
Marcelo Macedo <marcelomacedodev@gmail.com>
2021-09-25 22:37:21 +0100

    [NEW] Read data from json file [WIP] Create Rows to list of landmarks

---
+++ Landmarks/Model/ModelData.swift
@@ -0,0 +1,33 @@
+//
+//  ModelData.swift
+//  Landmarks
+//
+//  Created by Marcelo Macedo on 25/09/2021.
+//
+
+import Foundation
+
+var landmarks: [Landmark] = load("landmarkData.json")
+
+//function that reads from json
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+    else {
+        fatalError("Couldn't find \(filename) in main bundle.")
+    }
+
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+    }
+
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    }
+}
